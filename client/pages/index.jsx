7ec04535291c1a6734b58ad11a63e3c0f62b1bf7
0papi <evanskwofie67@gmail.com>
2023-02-10 10:49:13 +0000

    add client project

---
+++ client/pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello world, say hi to a cool landing page</h2>
+    </div>
+  );
+}
--- client/readme.md
+++ client/readme.md
@@ -1,1 +1,1 @@
-Client App
+# Project Client App
